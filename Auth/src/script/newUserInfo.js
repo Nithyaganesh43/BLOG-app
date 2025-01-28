f42645432c85f4225f57e59dc3a341d10ac6e07a
Nithya ganesh <nithyaganesh4343@gmail.com>
2025-01-28 13:01:23 +0530

    initial

---
+++ Auth/src/script/newUserInfo.js
@@ -0,0 +1,168 @@
+function myAlert(icon, title) {
+  Swal.fire({
+    toast: true,
+    position: 'top',
+    icon: icon,
+    title: title,
+    timer: 5000,
+    timerProgressBar: true,
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    showConfirmButton: false,
+    showCloseButton: true,
+    customClass: {
+      popup: 'custom-toast',
+    },
+    willOpen: () => {
+      const style = document.createElement('style');
+      style.innerHTML = `
+                        .custom-toast {
+                            font-family: 'Arial', sans-serif;
+                            font-size: 12px;
+                            background-color: rgb(255, 255, 255);
+                            color: #262626;
+                        }
+                    `;
+      document.head.appendChild(style);
+    },
+  });
+}
+window.onload = async function () {
+  const urlParams = new URLSearchParams(window.location.search);
+
+  const platform = urlParams.get('platform');
+  const email = urlParams.get('email');
+  let fullname = urlParams.get('fullname');
+
+  if ('undefined' == fullname) {
+    fullname = '';
+  }
+
+  if (fullname) document.getElementById('fullName').value = fullname;
+  if (email) {
+    const emailField = document.getElementById('email');
+    emailField.value = email;
+    emailField.readOnly = true;
+  }
+  if (platform) {
+    const platformField = document.getElementById('platform');
+    platformField.value = platform;
+    platformField.readOnly = true;
+  }
+
+  const defaultUrl =
+    'https://res.cloudinary.com/dmini3yl9/image/upload/v1730714916/di75th4l9fqebilewtur.avif';
+  const img = document.getElementById('profile-img');
+
+  async function loadProfileImage(url, fallbackUrl) {
+    try {
+      if (!url) throw new Error('Invalid URL');
+      const imgCheck = await new Promise((resolve, reject) => {
+        const tempImg = new Image();
+        tempImg.onload = () => resolve(true);
+        tempImg.onerror = () => reject();
+        tempImg.src = url;
+      });
+      if (imgCheck) img.setAttribute('src', url);
+    } catch {
+      img.setAttribute('src', fallbackUrl);
+    }
+  }
+
+  const profileUrl = urlParams.get('profileUrl');
+  loadProfileImage(profileUrl, defaultUrl);
+
+  const inputFields = document.querySelectorAll('input');
+  inputFields.forEach((input) => {
+    input.addEventListener('keydown', (event) => {
+      if (event.key === 'Enter') {
+        document.getElementById('submit-button').click();
+        event.preventDefault();
+      }
+    });
+  });
+  function validateInput(fullName, userName, password, confirmPassword) {
+    
+    if (fullName.length < 3) {
+      myAlert('error', 'Full name must be at least 3 characters long.');
+      return false;
+    }
+    if (fullName.length > 50) {
+      myAlert('error', 'Full name cannot exceed 50 characters.');
+      return false;
+    }
+
+    if (userName.length < 7) {
+      myAlert('error', 'Username must be at least 7 characters long.');
+      return false;
+    }
+    if (userName.length > 50) {
+      myAlert('error', 'Username cannot exceed 50 characters.');
+      return false;
+    }
+
+    if (password.length < 7) {
+      myAlert('error', 'Password must be at least 7 characters long.');
+      return false;
+    }
+    if (password.length > 50) {
+      myAlert('error', 'Password cannot exceed 50 characters.');
+      return false;
+    }
+    if (!/[A-Z]/.test(password)) {
+      myAlert('error', 'Password must include at least one uppercase letter.');
+      return false;
+    }
+    if (!/[0-9]/.test(password)) {
+      myAlert('error', 'Password must include at least one number.');
+      return false;
+    }
+
+    if (password !== confirmPassword) {
+      myAlert('error', 'Passwords do not match. Please try again.');
+      return false;
+    }
+
+    return true;
+  }
+
+  document.getElementById('submit-button').onclick = async function (event) {
+    event.preventDefault();
+    const fullName = document.getElementById('fullName').value;
+    const userName = document.getElementById('userName').value.trim();
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+myAlert('info','Validating');
+    if (validateInput(fullName, userName, password, confirmPassword)) {
+      await axios
+        .post(
+          'http://localhost:3000/auth/signupSuccessful',
+          { fullName, userName, password, email, platform },
+          {
+            withCredentials: true,
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        )
+        .then((response) => {
+          myAlert('success', response.data.message);
+          window.location.href =
+            'http://localhost:3000/auth/home';
+        })
+        .catch((error) => {
+          myAlert(
+            'error',
+            error.response?.data?.message || 'An error occurred'
+          );
+        });
+    }
+  };
+};
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    document.querySelector('.btnClick').click();
+  }
+});
+ 
---
+++ Home/src/index.js
@@ -0,0 +1,86 @@
+import React, { useEffect, lazy, Suspense } from 'react';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import ReactDOM from 'react-dom/client';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { GlobalStyle } from './globalStyles';
+import Contact from './components/Contact/index';
+
+const Header = lazy(() => import('./components/Header/index'));
+const TermsAndConditions = lazy(() =>
+  import('./Pages/TermsAndConditions/index')
+);
+const PrivacyPolicy = lazy(() => import('./Pages/PrivacyPolicy/index'));
+const Home = lazy(() => import('./Pages/Home'));
+
+const RootApp = () => {
+  // useEffect(() => {
+  //   const checkAuth = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         'https://server.markethealers.com/markethealers/auth/authCheck',
+  //         {
+  //           method: 'GET',
+  //           credentials: 'include',
+  //         }
+  //       );
+
+  //       if (response.ok) { 
+  //         toast.success('Successfully Logged In');
+  //       } else {
+  //         console.log('failed');
+  //         toast.success('Welcome To Market Healers. SignUp and Heal Your Finance Life Here');
+  //       }
+  //     } catch (error) {
+  //       console.error('Error checking auth:');
+  //     }
+  //   };
+
+  //   checkAuth();
+  // }, []);
+
+  return (
+    <>
+      <GlobalStyle />
+      <Suspense fallback={null}>
+        <Outlet />
+        <Contact />
+      </Suspense>
+      <ToastContainer position="top-right" autoClose={3000} />
+    </>
+  );
+};
+
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootApp />,
+    children: [
+      {
+        path: '/',
+        element: (
+          <>
+            <Header />
+            <Home />
+          </>
+        ),
+      },
+      {
+        path: '/termsofservice',
+        element: <TermsAndConditions />,
+      },
+      {
+        path: '/privacypolicy',
+        element: <PrivacyPolicy />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={appRouter} />
+  </React.StrictMode>
+);
